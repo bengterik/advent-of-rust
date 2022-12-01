f419e08910015dd0cbeee318b5335d3e38601a5f
Alexander <alexander.h.sandstrom@gmail.com>
2022-12-01 21:10:11 +0100

    Update gitignore

---
+++ 1/1.rs
@@ -0,0 +1,40 @@
+//use std::env;
+use std::fs;
+
+fn main() {
+    
+    let contents = fs::read_to_string("calories.in")
+        .expect("Should have been able to read file");
+    
+    //println!("{}", contents);
+
+    let splitted = contents.split("\n");
+    
+    let mut highest: [i32; 3] = [0, 0, 0]; 
+    let mut temp: i32 = 0;
+
+    for s in splitted {
+        match s {
+            "" => {
+                let (i, lowest) = index_of_lowest(&mut highest);
+                if lowest < temp { highest[i] = temp };
+                temp = 0;
+            },
+            _  => temp += s.parse::<i32>().unwrap(),
+        }
+    }
+    println!("{}", highest[0]+highest[1]+highest[2]);
+}
+
+fn index_of_lowest(arr: &mut [i32; 3]) -> (usize, i32) {
+    let mut n = 0;
+    let mut lowest = &std::i32::MAX;
+    for (i, cal) in arr.iter().enumerate() {
+        if cal < lowest {
+            n = i;
+            lowest = cal;
+        }
+    }
+    return (n, *lowest);
+
+}
